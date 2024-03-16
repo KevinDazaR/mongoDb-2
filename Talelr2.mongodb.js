91482c32048547040e42b884445ba81b789d45ba
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-03-15 22:34:44 -0500

    Add files via upload

---
+++ Talelr2.mongodb.js
@@ -0,0 +1,744 @@
+db.createCollection('users');
+
+db.users.insertMany([
+    {
+        names: 'John',
+        lastnames: 'Smith',
+        email: 'johnsmith@gmail.com',
+        city: 'New York',
+        country: 'United States',
+        salary: 3000,
+        age: 30,
+        height: 180,
+        weight: 170
+    },
+    {
+        names: 'Sophie',
+        lastnames: 'Dupont',
+        email: 'sophiedupont@gmail.com',
+        city: 'Paris',
+        country: 'France',
+        salary: 2800,
+        age: 28,
+        height: 165,
+        weight: 140
+    },
+    {
+        names: 'Carlos',
+        lastnames: 'Rodriguez',
+        email: 'carlosrodriguez@gmail.com',
+        city: 'Buenos Aires',
+        country: 'Argentina',
+        salary: 2500,
+        age: 25,
+        height: 175,
+        weight: 150
+    },
+    {
+        names: 'Maria',
+        lastnames: 'Gonzalez',
+        email: 'mariagonzalez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 3200,
+        age: 32,
+        height: 170,
+        weight: 160
+    },
+    {
+        names: 'Hiroshi',
+        lastnames: 'Tanaka',
+        email: 'hiroshitanaka@gmail.com',
+        city: 'Tokyo',
+        country: 'Japan',
+        salary: 4000,
+        age: 35,
+        height: 175,
+        weight: 155
+    },
+        {
+        names: 'Luis',
+        lastnames: 'Martinez',
+        email: 'luismartinez@gmail.com',
+        city: 'Mexico City',
+        country: 'Mexico',
+        salary: 2600,
+        age: 27,
+        height: 178,
+        weight: 165
+    },
+    {
+        names: 'Anna',
+        lastnames: 'Andersen',
+        email: 'annaandersen@gmail.com',
+        city: 'Copenhagen',
+        country: 'Denmark',
+        salary: 3200,
+        age: 29,
+        height: 170,
+        weight: 150
+    },
+    {
+        names: 'Andrea',
+        lastnames: 'Rossi',
+        email: 'andrearossi@gmail.com',
+        city: 'Rome',
+        country: 'Italy',
+        salary: 2900,
+        age: 31,
+        height: 175,
+        weight: 160
+    },
+    {
+        names: 'Miguel',
+        lastnames: 'Santos',
+        email: 'miguelsantos@gmail.com',
+        city: 'Lisbon',
+        country: 'Portugal',
+        salary: 2700,
+        age: 28,
+        height: 180,
+        weight: 170
+    },
+    {
+        names: 'Sara',
+        lastnames: 'Kovač',
+        email: 'sarakovac@gmail.com',
+        city: 'Zagreb',
+        country: 'Croatia',
+        salary: 3000,
+        age: 30,
+        height: 165,
+        weight: 145
+    },    
+    {
+        names: 'Emily',
+        lastnames: 'Johnson',
+        email: 'emilyjohnson@gmail.com',
+        city: 'London',
+        country: 'United Kingdom',
+        salary: 2800,
+        age: 28,
+        height: 168,
+        weight: 140
+    },
+    {
+        names: 'Andrea',
+        lastnames: 'Martinez',
+        email: 'andreamartinez@gmail.com',
+        city: 'Mexico City',
+        country: 'Mexico',
+        salary: 2600,
+        age: 26,
+        height: 170,
+        weight: 150
+    },
+    {
+        names: 'Lucas',
+        lastnames: 'Silva',
+        email: 'lucassilva@gmail.com',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+        salary: 2900,
+        age: 29,
+        height: 175,
+        weight: 160
+    },
+    {
+        names: 'Emma',
+        lastnames: 'Brown',
+        email: 'emmabrown@gmail.com',
+        city: 'Sydney',
+        country: 'Australia',
+        salary: 3100,
+        age: 31,
+        height: 172,
+        weight: 145
+    },
+    {
+        names: 'Antonio',
+        lastnames: 'Perez',
+        email: 'antonioperez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 2700,
+        age: 27,
+        height: 178,
+        weight: 155
+    },
+    {
+        names: 'Sophia',
+        lastnames: 'Garcia',
+        email: 'sophiagarcia@gmail.com',
+        city: 'Bogotá',
+        country: 'Colombia',
+        salary: 2700,
+        age: 26,
+        height: 168,
+        weight: 145
+    },
+    {
+        names: 'Liam',
+        lastnames: 'Jones',
+        email: 'liamjones@gmail.com',
+        city: 'Melbourne',
+        country: 'Australia',
+        salary: 3200,
+        age: 30,
+        height: 180,
+        weight: 165
+    },
+    {
+        names: 'Isabella',
+        lastnames: 'Martinez',
+        email: 'isabellamartinez@gmail.com',
+        city: 'Santiago',
+        country: 'Chile',
+        salary: 2800,
+        age: 28,
+        height: 170,
+        weight: 150
+    },
+    {
+        names: 'Matías',
+        lastnames: 'Fernández',
+        email: 'matiasfernandez@gmail.com',
+        city: 'Buenos Aires',
+        country: 'Argentina',
+        salary: 3000,
+        age: 29,
+        height: 175,
+        weight: 160
+    },
+    {
+        names: 'Ava',
+        lastnames: 'Wilson',
+        email: 'avawilson@gmail.com',
+        city: 'Los Angeles',
+        country: 'United States',
+        salary: 2900,
+        age: 27,
+        height: 172,
+        weight: 155
+    },
+    {
+        names: 'Oliver',
+        lastnames: 'Smith',
+        email: 'oliversmith@gmail.com',
+        city: 'London',
+        country: 'United Kingdom',
+        salary: 3000,
+        age: 28,
+        height: 175,
+        weight: 150
+    },
+    {
+        names: 'Emma',
+        lastnames: 'Brown',
+        email: 'emmabrown@gmail.com',
+        city: 'New York',
+        country: 'United States',
+        salary: 3200,
+        age: 30,
+        height: 168,
+        weight: 140
+    },
+    {
+        names: 'Lucas',
+        lastnames: 'Silva',
+        email: 'lucassilva@gmail.com',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+        salary: 2800,
+        age: 27,
+        height: 180,
+        weight: 160
+    },
+    {
+        names: 'Isabella',
+        lastnames: 'Garcia',
+        email: 'isabellagarcia@gmail.com',
+        city: 'Bogota',
+        country: 'Colombia',
+        salary: 3100,
+        age: 31,
+        height: 170,
+        weight: 145
+    },
+    {
+        names: 'Hiroshi',
+        lastnames: 'Tanaka',
+        email: 'hiroshitanaka@gmail.com',
+        city: 'Tokyo',
+        country: 'Japan',
+        salary: 4000,
+        age: 35,
+        height: 175,
+        weight: 155
+    },
+    {
+        names: 'Mia',
+        lastnames: 'Martinez',
+        email: 'miamartinez@gmail.com',
+        city: 'Buenos Aires',
+        country: 'Argentina',
+        salary: 2900,
+        age: 28,
+        height: 165,
+        weight: 135
+    },
+    {
+        names: 'Sebastian',
+        lastnames: 'Gomez',
+        email: 'sebastiangomez@gmail.com',
+        city: 'Santiago',
+        country: 'Chile',
+        salary: 3100,
+        age: 29,
+        height: 172,
+        weight: 155
+    },
+    {
+        names: 'Ava',
+        lastnames: 'Taylor',
+        email: 'avataylor@gmail.com',
+        city: 'Sydney',
+        country: 'Australia',
+        salary: 3300,
+        age: 32,
+        height: 168,
+        weight: 150
+    },
+    {
+        names: 'Antonio',
+        lastnames: 'Lopez',
+        email: 'antoniolopez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 2700,
+        age: 27,
+        height: 175,
+        weight: 160
+    },
+    {
+        names: 'Charlotte',
+        lastnames: 'Johnson',
+        email: 'charlottejohnson@gmail.com',
+        city: 'Paris',
+        country: 'France',
+        salary: 3000,
+        age: 30,
+        height: 170,
+        weight: 145
+    },
+    {
+        names: 'Liam',
+        lastnames: 'Williams',
+        email: 'liamwilliams@gmail.com',
+        city: 'Toronto',
+        country: 'Canada',
+        salary: 2800,
+        age: 29,
+        height: 185,
+        weight: 170
+    },
+    {
+        names: 'Sophia',
+        lastnames: 'Brown',
+        email: 'sophiabrown@gmail.com',
+        city: 'Berlin',
+        country: 'Germany',
+        salary: 2900,
+        age: 28,
+        height: 166,
+        weight: 140
+    },
+    {
+        names: 'Daniel',
+        lastnames: 'Garcia',
+        email: 'danielgarcia@gmail.com',
+        city: 'Mexico City',
+        country: 'Mexico',
+        salary: 2600,
+        age: 26,
+        height: 172,
+        weight: 155
+    },
+    {
+        names: 'Emma',
+        lastnames: 'Smith',
+        email: 'emmasmith@gmail.com',
+        city: 'Rio de Janeiro',
+        country: 'Brazil',
+        salary: 3100,
+        age: 31,
+        height: 168,
+        weight: 145
+    },
+    {
+        names: 'Lucas',
+        lastnames: 'Fernandez',
+        email: 'lucasfernandez@gmail.com',
+        city: 'Barcelona',
+        country: 'Spain',
+        salary: 3200,
+        age: 32,
+        height: 180,
+        weight: 160
+    }
+
+]);
+
+db.users.count();
+
+
+/* Operaciones de Eliminación */
+
+
+// Eliminar todos los usuarios que tienen un salario menor que 2000 dólares.
+
+db.users.insertMany([
+    {
+        names: 'Lucas',
+        lastnames: 'Fernandez',
+        email: 'lucasfernandez@gmail.com',
+        city: 'Barcelona',
+        country: 'Spain',
+        salary: 1800,
+        age: 32,
+        height: 180,
+        weight: 160
+    },
+    {
+        names: 'Sophia',
+        lastnames: 'Gomez',
+        email: 'sophiagomez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 1500,
+        age: 25,
+        height: 165,
+        weight: 140
+    },
+    {
+        names: 'Carlos',
+        lastnames: 'Rodriguez',
+        email: 'carlosrodriguez@gmail.com',
+        city: 'Valencia',
+        country: 'Spain',
+        salary: 1900,
+        age: 28,
+        height: 175,
+        weight: 150
+    }
+    // Agregar más usuarios con salarios menores a 2000 aquí
+]);
+
+db.users.find( { salary: { $lt: 2000 }})
+
+db.users.deleteMany( { salary: { $lt: 2000}});
+
+
+// Eliminar usuarios que tienen una edad menor que 25 años.
+
+db.users.find({ age: {$lt: 25}});
+
+db.users.deleteMany( { age: { $lt: 25}});
+
+// Borrar todos los usuarios que viven en "París".
+
+db.users.find({ city: 'Paris'});
+db.users.deleteMany( { city: 'Paris'});
+
+
+// Eliminar usuarios que tienen un peso superior a 180 libras.
+
+db.users.insertMany([
+    {
+        names: 'Sophie',
+        lastnames: 'Garcia',
+        email: 'sophiegarcia@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 2200,
+        age: 29,
+        height: 170,
+        weight: 185
+    },
+    {
+        names: 'Daniel',
+        lastnames: 'Perez',
+        email: 'danielperez@gmail.com',
+        city: 'Barcelona',
+        country: 'Spain',
+        salary: 2100,
+        age: 30,
+        height: 178,
+        weight: 190
+    },
+    {
+        names: 'Isabella',
+        lastnames: 'Martinez',
+        email: 'isabellamartinez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 2300,
+        age: 27,
+        height: 165,
+        weight: 182
+    },
+    {
+        names: 'David',
+        lastnames: 'Gomez',
+        email: 'davidgomez@gmail.com',
+        city: 'Valencia',
+        country: 'Spain',
+        salary: 2400,
+        age: 33,
+        height: 175,
+        weight: 185
+    }
+]);
+db.users.count( { weight: { $gt: 180}});
+
+db.users.deleteMany( { weight: {$gt: 180}});
+
+
+// Eliminar usuarios que tienen una altura inferior a 160 centímetros.
+
+db.users.insertMany([
+    {
+        names: 'Maria',
+        lastnames: 'Gonzalez',
+        email: 'mariagonzalez@gmail.com',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 1900,
+        age: 26,
+        height: 155,
+        weight: 160
+    },
+    {
+        names: 'Juan',
+        lastnames: 'Lopez',
+        email: 'juanlopez@gmail.com',
+        city: 'Barcelona',
+        country: 'Spain',
+        salary: 2000,
+        age: 24,
+        height: 158,
+        weight: 170
+    }
+]);
+
+db.users.count({ height: {$lt: 160}});
+
+
+// Eliminar todos los usuarios que tienen el nombre "John" y el apellido "Doe".
+
+db.users.insertOne({
+    names: 'Jhon',
+    lastnames: 'Doe',
+    email: 'mariagonzalez@gmail.com',
+    city: 'Madrid',
+    country: 'Spain',
+    salary: 1900,
+    age: 26,
+    height: 155,
+    weight: 160
+})
+
+db.users.count({ names: 'Jhon'}, {lastnames: 'Doe'});
+
+
+// Borrar usuarios que tienen una dirección de correo electrónico específica, por ejemplo, "borrar@riwi.com".
+
+db.users.find({ email: 'johnsmith@gmail.com'});
+db.users.deleteMany({ email: 'johnsmith@gmail.com'});
+
+// Eliminar usuarios que no tienen una dirección de correo electrónico registrada.
+
+
+db.users.insertOne(
+    {
+        names: 'Sophie',
+        lastnames: 'Garcia',
+        city: 'Madrid',
+        country: 'Spain',
+        salary: 2200,
+        age: 29,
+        height: 170,
+        weight: 185
+    }
+);
+
+db.users.deleteMany({ email: { $exists: false } });
+
+
+// Eliminar usuarios que viven en "Tokyo".
+
+db.users.count({city: 'Tokyo'});
+
+db.users.deleteMany({city: 'Tokyo'});
+
+
+
+// Borrar todos los usuarios que son menores de 18 años.
+db.users.insertOne({
+    names: "María",
+    lastnames: "González",
+    email: "maria.gonzalez@example.com",
+    city: "Madrid",
+    country: "Spain",
+    salary: 1800,
+    age: 16,
+    height: 160,
+    weight: 50
+});
+
+db.users.count({ age: {$lt: 18}});
+db.users.deleteMany({ age: {$lt: 18}});
+
+
+
+// Eliminar usuarios que tienen un salario igual a 0.
+
+db.users.insertOne({
+    names: "Juan",
+    lastnames: "López",
+    email: "juan.lopez@example.com",
+    city: "Barcelona",
+    country: "Spain",
+    salary: 0,
+    age: 20,
+    height: 170,
+    weight: 60
+});
+
+db.users.count( { salary: {$eq: 0}});
+db.users.deleteMany( { salary: {$eq: 0}});
+
+
+// Borrar usuarios que viven en "New York" y tienen un salario superior a 5000 dólares.
+
+db.users.count( { city: 'New York'}, { salary: {$gt: 5000}});
+db.users.deleteMany( { city: 'New York'}, { salary: {$gt: 5000}});
+
+// Eliminar usuarios que tienen una dirección de correo electrónico que contiene la palabra "spam".
+
+db.users.insertOne({
+    names: "Laura",
+    lastnames: "Spamson",
+    email: "laura.spamson@example.com",
+    city: "Madrid",
+    country: "Spain",
+    salary: 2000,
+    age: 25,
+    height: 165,
+    weight: 55
+});
+
+
+db.users.count({ email: { $regex: /spam/ }});
+db.users.deleteMany({ email: { $regex: /spam/ }});
+
+// Borrar usuarios que viven en "Bello" y tienen más de 50 años.
+
+db.users.insertOne({
+    names: "Roberto",
+    lastnames: "González",
+    email: "roberto.gonzalez@example.com",
+    city: "Bello",
+    country: "Colombia",
+    salary: 2500,
+    age: 55,
+    height: 170,
+    weight: 75
+});
+
+db.users.count({ city: 'Bello' }, { age: {$gt: 50}});
+
+db.users.deleteOne({ city: 'Bello' }, { age: {$gt: 50}});
+
+// Eliminar todos los usuarios que tienen el apellido "González".
+
+db.users.insertOne({
+    names: "Carlos",
+    lastnames: "González",
+    email: "carlos.gonzalez@example.com",
+    city: "Medellín",
+    country: "Colombia",
+    salary: 3000,
+    age: 40,
+    height: 175,
+    weight: 70
+});
+
+db.users.count({ lastnames: 'González'});
+
+db.users.deleteMany({ lastnames: 'González'});
+
+// Borrar usuarios que tienen una altura inferior a 160 centímetros.
+
+
+db.users.deleteMany({ height: {$lt: 160}});
+
+// Borrar usuarios que tienen una edad superior a 70 años.
+
+db.users.deleteMany({ age: {$gt: 70}});
+
+
+// Eliminar usuarios que tienen un país de residencia igual a "Canadá" y un salario inferior a 4000 dólares.
+
+db.users.count({ country: 'Canada'}, { salary: {$lt: 4000}});
+
+// Borrar usuarios que tienen un salario entre 1000 y 2000 dólares.
+
+db.users.insertMany([
+    {
+        names: "Ana",
+        lastnames: "Martínez",
+        email: "ana.martinez@example.com",
+        city: "Barcelona",
+        country: "Spain",
+        salary: 1500,
+        age: 35,
+        height: 165,
+        weight: 60
+    },
+    {
+        names: "Pedro",
+        lastnames: "Sánchez",
+        email: "pedro.sanchez@example.com",
+        city: "Madrid",
+        country: "Spain",
+        salary: 1800,
+        age: 28,
+        height: 170,
+        weight: 70
+    }
+]);
+
+db.users.count( { salary: {$gt: 1000, $lt:2000}});
+db.users.deleteMany( { salary: {$gt: 1000, $lt:2000}});
+
+// Eliminar usuarios que tienen una edad igual a 30 años.
+
+db.users.count({ age: 30});
+db.users.deleteMany({ age: 30});
+
+// Borrar usuarios que tienen una altura superior a 190 centímetros.
+db.users.insertOne({
+    names: "Juan",
+    lastnames: "Gómez",
+    email: "juan.gomez@example.com",
+    city: "Madrid",
+    country: "Spain",
+    salary: 2200,
+    age: 30,
+    height: 195,
+    weight: 80
+});
+
+db.users.deleteMany({ height: {$gt: 190}});
+
